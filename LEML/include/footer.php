1a49f4de261df2887c6363a73d7156f0a586909c
Andy Martin <andymartin51@gmail.com>
2015-01-25 14:28:21 -0700

    change from php to html, remove unnecessary pages from webhost

--- LEML/include/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="container_12"><div class="grid_12 divider"></div></div>
-<div class="container_12">
-	<div class="grid_12" align="center" id="footer">
-		Copyright &copy; <script>var today = new Date(); var year = today.getFullYear(); document.write(year);</script> Little Elk Meadows Lodge. 
-		All Rights Reserved. 
-		Website design by <a target="_blank" href="http://www.logcabincomputers.com">Log Cabin Computers</a>
-		<p></p>
-	</div>
-</div>
